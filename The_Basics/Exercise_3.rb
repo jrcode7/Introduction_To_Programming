0ac8746940c41cb1ab92e5f9ff4bb14d8a131026
Jesus Rivera <jr092291@gmail.com>
2017-10-07 09:53:29 -0700

    Adding all exercises from 'The Basics' chapter

---
+++ The_Basics/Exercise_3.rb
@@ -0,0 +1,13 @@
+movies = {
+  :Blade_Runner => "1982",
+  :Alien => "1979",
+  :Evil_Dead => "1981",
+  :A_Nightmare_On_Elm_Street => "1984",
+  :It => "1990"
+}
+
+puts movies[:Blade_Runner]
+puts movies[:Alien]
+puts movies[:Evil_Dead]
+puts movies[:A_Nightmare_On_Elm_Street]
+puts movies[:It]
---
+++ The_Basics/Exercise_4.rb
@@ -0,0 +1,7 @@
+movies = [1982, 1979, 1981, 1984, 1990]
+
+puts movies[0]
+puts movies[1]
+puts movies[2]
+puts movies[3]
+puts movies[4]
